e99cd37a2b9591c31e447e01e670d6d8cfd7485f
新ユーザ名 <14d4010002km@edu.tech.ac.jp>
2015-05-13 05:51:43 +0000

    -

---
+++ practice.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $myname = 'ayane';
+    print($myname);
+    print('は');
+    print(strlen($myname));
+    print('文字です');
+    ?>   
+    <br>
+    <?php
+    echo($myname);
+    echo('は');
+    echo(strlen($myname));
+    echo('文字です');
+    ?>
+</body>
+</html>
---
+++ sample06.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = htmlspecialchars($_REQUEST['my_name'],ENT_QUOTES);
+    ?>
+    <p>ようこそ<?php echo($name);?>さん。</p>
+    <ul>
+        <li>
+            <a href="#"><?php print($name);?>さんのマイページを見る</a>
+        </li>
+    </ul>
+</body>
+</html>
