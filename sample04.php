dba3b1a6a45832bbd80036799be4c06e17795292
新ユーザ名 <14d4010002km@edu.tech.ac.jp>
2015-05-20 04:38:32 +0000

    -

---
+++ sample04.php
@@ -0,0 +1,13 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+    print('お名前:'.htmlspecialchars($_REQUEST['my_name'],ENT_QUOTES));
+    ?> 
+</body>
+</html>
